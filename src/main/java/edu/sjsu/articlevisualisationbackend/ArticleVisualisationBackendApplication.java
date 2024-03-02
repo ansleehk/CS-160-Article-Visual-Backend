a92cd27cf2ad984e9e58b9bc3e9d49652ca5e374
leeanson19 <leeanson19@gmail.com>
2024-03-02 12:23:09 -0800

    Setup Environment

---
+++ src/main/java/edu/sjsu/articlevisualisationbackend/ArticleVisualisationBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.articlevisualisationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleVisualisationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleVisualisationBackendApplication.class, args);
+	}
+
+}
