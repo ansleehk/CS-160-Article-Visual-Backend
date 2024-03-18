b986b9f814e25c19af335a4fa523d1e38047ab6f
leeanson19 <leeanson19@gmail.com>
2024-03-18 11:24:56 -0700

    Temp remove CORS config

--- src/main/java/edu/sjsu/articlevisualisationbackend/config/CorsConfig.java
+++ src/main/java/edu/sjsu/articlevisualisationbackend/config/CorsConfig.java
@@ -4,15 +4,3 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer{
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Matches all application routes
-                .allowedOrigins("*") // Allows all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*") // Allows all headers
-                .allowCredentials(true)
-                .maxAge(3600); // Cache duration (seconds)
-    }
-}
